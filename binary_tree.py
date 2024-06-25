bb45397a5c7144ce5e0980b8f8f7446784d1eeb1
mayyshabaneh <mayyiztech@gmail.com>
2024-06-25 15:57:50 +0300

    comment with time comp

--- binary_tree.py
+++ binary_tree.py
@@ -9,7 +9,7 @@ class BinTree:
     def __init__(self) -> None:
         self.root = None
     
-    def insert(self, value):
+    def insert(self, value):#time comp = O(N) / N : the number of nodes
         node = Node(value)
         if self.root is None:
             self.root = node
@@ -31,7 +31,7 @@ class BinTree:
                 queue.append(temp.right)
 
     
-    def inorder_traversal(self, root):
+    def inorder_traversal(self, root):#time comp = O(N) / N : the number of nodes
         result = []
         if root:
             result = self.inorder_traversal(root.left)
@@ -40,7 +40,7 @@ class BinTree:
         return result
     
 
-    def preorder_traversal(self, root):
+    def preorder_traversal(self, root):#time comp = O(N) / N : the number of nodes
         result = []
         if root:
             result.append(root.value)
@@ -49,7 +49,7 @@ class BinTree:
         return result
     
 
-    def postorder_traversal(self, root):
+    def postorder_traversal(self, root):#time comp = O(N) / N : the number of nodes
         result = []
         if root:
             result = self.postorder_traversal(root.left)
@@ -58,7 +58,7 @@ class BinTree:
         return result
 
 
-    def search(self,value):
+    def search(self,value):#time comp = O(N) / N : the number of nodes
         if value in self.inorder_traversal(self.root) :
             print ("value exist")
             return True
@@ -67,20 +67,24 @@ class BinTree:
             return False
 
 
-    def find_min(self):
+    def find_min(self):#time comp = O(N) / N : the number of nodes
        return min(self.inorder_traversal(self.root))
 
 
-    def find_max(self):
+    def find_max(self):#time comp = O(N) / N : the number of nodes
         return max(self.inorder_traversal(self.root))
 
 
     def delete(self,value):
-        if self.search(value) is True:
-            value.left =value.left.left
-            value.left.right = value.right
-        else :
-            print ("value does not exisit")
+        if self.search(value) is True :
+            temp = self.root
+            while self.root :
+                if temp.value == value:
+                    del(temp.value)
+                    return
+                else :
+                    print ("the value dosnt exist or the tree is empty")
+                return
 
 
 
@@ -103,7 +107,5 @@ tree.search(9)
 tree.search(10)
 print("the minimum value is : " , tree.find_min())
 print("the maximum value is : " , tree.find_max())
-tree.delete(9)
-print("inorder traversal --> ",tree.inorder_traversal(tree.root))
-print("postorder traversal --> ",tree.postorder_traversal(tree.root))
-print("preorder traversal --> ",tree.preorder_traversal(tree.root))
+tree.delete(5)
+tree.search(5)
